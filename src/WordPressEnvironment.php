b8ecbc6e277120825024be5cd06d15773cfc7085
Evan Hildreth <me@eph.me>
2025-03-22 13:21:20 -0400

    More environmental info

---
+++ src/WordPressEnvironment.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Smolblog\WP;
+
+use Smolblog\Foundation\Value\Fields\{Identifier, RandomIdentifier, Url};
+
+class WordPressEnvironment {
+	public function getAdminUrl(string $key): Url {
+		return new Url(get_admin_url(null, 'admin.php?page=' . $key));
+	}
+
+	public function getUserId(?int $wordPressId = null): Identifier {
+		$dbId = $wordPressId ?? get_current_user_id();
+		if ($dbId < 1) {
+			return Identifier::nil();
+		}
+
+		$meta_value = get_user_meta( $dbId, 'smolblog_user_id', true );
+
+		if (empty($meta_value)) {
+			$new_id = new RandomIdentifier();
+			update_user_meta($dbId, 'smolblog_user_id', $new_id->toString());
+
+			return $new_id;
+		}
+
+		return Identifier::fromString($meta_value);
+	}
+
+	public function getSiteId(?int $wordPressId = null): Identifier {
+		if (is_multisite()) {
+			return $this->getSiteIdMultisite($wordPressId);
+		}
+		return $this->getSiteIdSingle();
+	}
+
+	private function getSiteIdSingle(): Identifier {
+		$optionValue = get_option('smolblog_site_id');
+		if ($optionValue === false) {
+			// If the site does not have an ID, give it one.
+			$new_id = new RandomIdentifier();
+			add_option('smolblog_site_id', $new_id->toString());
+
+			return $new_id;
+		}
+
+		return Identifier::fromString($optionValue);
+	}
+
+	private function getSiteIdMultisite(?int $wordPressId = null): Identifier {
+		$dbId = $wordPressId ?? get_current_blog_id();
+		if ($dbId < 1) {
+			return Identifier::nil();
+		}
+
+		$optionValue = get_blog_option($dbId, 'smolblog_site_id', false);
+		if ($optionValue === false) {
+			// If the site does not have an ID, give it one.
+			$new_id = new RandomIdentifier();
+			add_blog_option($dbId, 'smolblog_site_id', $new_id->toString());
+
+			return $new_id;
+		}
+
+		return Identifier::fromString($optionValue);
+	}
+}
