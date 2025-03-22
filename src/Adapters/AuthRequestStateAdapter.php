93c9e1f19c0096272334e87130363312427c4781
Evan Hildreth <me@eph.me>
2025-03-22 13:21:38 -0400

    Bring over AuthRequestState

---
+++ src/Adapters/AuthRequestStateAdapter.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Smolblog\WP\Adapters;
+
+use Smolblog\Core\Connection\Data\AuthRequestStateRepo;
+use Smolblog\Core\Connection\Entities\AuthRequestState;
+
+/**
+ * Helper class to link WordPress and Smolblog transients.
+ */
+class AuthRequestStateAdapter implements AuthRequestStateRepo {
+	/**
+	 * Get the indicated AuthRequestState from the repository. Should return null if not found.
+	 *
+	 * @param string $key Unique identifier for the object.
+	 * @return AuthRequestState State identified by $id; null if it does not exist.
+	 */
+	public function getAuthRequestState(string $key): ?AuthRequestState {
+		$state = get_transient($key);
+		if (! is_array($state)) {
+			return null;
+		}
+
+		return AuthRequestState::deserializeValue($state);
+	}
+
+	/**
+	 * Save the given AuthRequestState
+	 *
+	 * @param AuthRequestState $state State to save.
+	 * @return void
+	 */
+	public function saveAuthRequestState(AuthRequestState $state): void {
+		set_transient($state->key, $state->serializeValue(), 60 * 15);
+	}
+}
